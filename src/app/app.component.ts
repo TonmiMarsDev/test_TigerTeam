c6bc11d33ef53b15b1aa9ed3f8e422dbf9b3cc5c
AntonioDev <antonio.martinez@externos-cl.cencosud.com>
2022-04-21 12:40:12 -0500

    se avanza

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tigerTeam_test';
+  title = 'Programming Languages';
+  languageNames : Array< string > = [];
+  language : any ;
+
+  public formPArent : FormGroup = new FormGroup({
+
+  });
+
+  @ViewChild('name') inputName: any;
+
+  onKeypress(event: any){
+    this.language = event.target.value;
+  
+    if(!!this.language.length){
+      this.languageNames.push(this.language);
+    }
+
+  }
+
+  handleClear(){
+    this.inputName.nativeElement.value = '';
+    // this.inputName.placeholder = '';
+  }
+
 }
